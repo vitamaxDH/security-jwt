a98a6cb6cb64b54b00ff9f5226eeee378636e4a5
vitamaxDH <hellonewtry@gmail.com>
2020-11-04 23:57:50 +0900

    chore: SecurityConfig 설정 완료

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -18,12 +18,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // cookie, session 등을 이용한 stateful x
                 .and()
                 // CrossOrigin 정책에서 벗어나겠다고 선언
                 .addFilter(corsFilter) // @CrossOrigin(인증x), 시큐리티 필터에 등록 인증(o)
-                .formLogin().disable()  // 폼으로 로그인 하는것 안함
-                .httpBasic().disable()
+                .formLogin().disable()  // jwt 설정 시 필수, 폼으로 로그인 하는것 안함
+                .httpBasic().disable()  // jwt 설정 시 필수. 왜냐? header 의 authorization 에 bearer 토큰을 넣을 것이기 때문
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")

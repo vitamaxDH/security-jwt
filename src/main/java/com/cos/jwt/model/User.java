6c2b0487ce71a524774ddaada33458266a292233
vitamaxDH <hellonewtry@gmail.com>
2020-11-04 23:31:20 +0900

    first init

---
+++ src/main/java/com/cos/jwt/model/User.java
@@ -0,0 +1,28 @@
+package com.cos.jwt.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@Entity
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    private String roles; // USER, ADMIN
+
+    public List<String> getRoleList(){
+        if(this.roles.length() > 0){
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+}

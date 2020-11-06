c64efb9341c264f0fecdbb76a82e3aa70a244f69
vitamaxDH <hellonewtry@gmail.com>
2020-11-06 22:03:34 +0900

    feat: deleted unnecesary import

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.cos.jwt.config;
 
-import com.cos.jwt.filter.MyFilter1;
 import com.cos.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 

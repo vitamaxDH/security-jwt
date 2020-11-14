385a4c6f93eca59a2dff886e186524153110807a
vitamaxDH <hellonewtry@gmail.com>
2020-11-14 19:00:42 +0900

    feat: Spring Security 로그인을 위한 기능 추가

---
+++ src/main/java/com/cos/jwt/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,27 @@
+package com.cos.jwt.config.auth;
+
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// http://localhost:8080/login  -> 기본 default
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("PrincipalDetailsService username :: {}", username);
+        User userEntity = userRepository.findByUsername(username);
+        return new PrincipalDetails(userEntity);
+    }
+}

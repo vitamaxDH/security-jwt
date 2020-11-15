c6d7881ac70844f3472cadbcddab65833628ba3e
vitamaxDH <hellonewtry@gmail.com>
2020-11-15 23:30:56 +0900

    feat: attemptAuthentication 추가

--- src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/cos/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,37 @@
 package com.cos.jwt.config.jwt;
 
-public class JwtAuthenticationFilter {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 스프링 시큐리티에 UsernamePasswordAuthenticationFilter 가 있음
+// login 요청해서 username, password 전송하면 (post)
+// UsernamePasswordAuthenticationFilter 가 동작함
+// 하지만 formLogin을 disable 해서 동작안함
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    // /login 요청을 하면 로그인 시도를 위해서 실행되는 함수
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        log.info("JwtAuthenticationFilter: 로그인 시도중");
+        // 1. username, password 받아서
+
+        // 2. 정상인지 로그인 시도. authenticationManager로 로그인 시도를 하면
+        // PrincipalDetailsService가 호출 -> loadByUsername() 호출
+
+        // 3. PrincipalDetails를 세션에 담고
+
+        // 4. JWT 토큰을 만들어서 응답해주면 됨.
+        return super.attemptAuthentication(request, response);
+    }
 }

b03bd699e672a3960ed049002b63f9cf57534cf1
vitamaxDH <hellonewtry@gmail.com>
2020-11-17 00:35:19 +0900

    feat: 테스트용 api 구현

--- src/main/java/com/cos/jwt/controller/RestApiController.java
+++ src/main/java/com/cos/jwt/controller/RestApiController.java
@@ -1,9 +1,11 @@
 package com.cos.jwt.controller;
 
+import com.cos.jwt.config.auth.PrincipalDetails;
 import com.cos.jwt.model.User;
 import com.cos.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +39,25 @@ public class RestApiController {
         log.info("User : {}", user);
 
         return "회원가입 완료";
+    }
+
+    // user, manager, admin 접근 가능
+    @GetMapping("/api/v1/user")
+    public String user(Authentication authentication){
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        log.info("authentication : {}", principalDetails.getUsername());
+        return "user";
+    }
+
+    // manager, admin 접근 가능
+    @GetMapping("/api/v1/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    // admin 접근 가능
+    @GetMapping("/api/v1/admin")
+    public String admin(){
+        return "admin";
     }
 }
